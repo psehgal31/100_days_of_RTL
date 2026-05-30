6399f79bcc4ab9bfd4f9b6a2b649f82bcf479811
psehgal31 <97.pranav31@gmail.com>
2026-05-30 08:56:55 +0000

    Understanding mailbox communication

---
+++ DAY25/mailboxes.sv
@@ -0,0 +1,50 @@
+module Mailboxes();
+  
+  mailbox pong;
+  mailbox ping;
+  
+  int sum;
+  
+  // Communicate between two initial blocks
+  
+  initial begin
+    pong = new();
+    ping = new();
+  end
+  
+  initial begin
+    sum = 0;
+    forever begin
+      // Wait for mailbox
+      ping.get(sum);
+      $display("%t: Sum: 0x%8x", $time, sum);
+      // Wait for some time
+      #1;
+      // Increment sum
+      sum++;
+      // Put the new sum into the mailbox
+      pong.put(sum);
+    end
+  end
+  
+  initial begin
+    forever begin
+      // Wait for some time
+      #1;
+      // Put into the ping mailbox
+      ping.put(sum);
+      // Wait for the pong mailbox
+      pong.get(sum);
+      // Increment the sum
+      sum++;
+      $display("%t: Sum: 0x%8x", $time, sum);
+    end
+  end
+  
+  // Finish sim in some time
+  initial begin
+    #200;
+    $finish();
+  end
+  
+endmodule
