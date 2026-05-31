33d3d5aed6585fc210d9473485f0f9db8c635bac
psehgal31 <97.pranav31@gmail.com>
2026-05-31 16:05:45 +0000

    Diff btwn fn &Tasks

---
+++ DAY26/func_tasks.sv
@@ -0,0 +1,31 @@
+// Function vs tasks
+
+package day26_pkg;
+
+function void print (logic[31:0] sum);
+  $display("%t 0x%8x", $time, sum);
+endfunction
+
+task increment_after_delay (input logic[31:0] sum);
+  // Wait for some time
+  #5;
+  // Print the incremented value
+  print(sum+1);
+  
+endtask
+endpackage
+
+module day26_tb();
+  
+  // Import the package
+  import day26_pkg::*;
+  
+  // Call the task in an initial block
+  initial begin
+    for (int i=0; i<512; i++) begin
+      increment_after_delay (i);
+    end
+    $finish();
+  end
+  
+endmodule
