2a5926b06e3b61f31474768086a2b7b3d4cf91f7
psehgal31 <97.pranav31@gmail.com>
2026-06-07 07:26:20 +0000

    Class based verif tb for DUT:ALU

---
+++ DAY33/testbench.sv
@@ -0,0 +1,26 @@
+`include "day4_test.sv"
+module day41_tb();
+  
+  // Instantiate the interface
+  day4_if intf();
+  
+  // Instantiate the RTL
+  ALU DAY4 (
+    .a_i		(intf.a),
+    .b_i		(intf.b),
+    .op_i		(intf.op),
+    .alu_o		(intf.alu)
+  );
+  
+  // Create the test class
+  day4_test test;
+  
+  initial begin
+    test = new;
+    test.env.vif = intf;
+    test.run();
+    $finish();
+  end
+  
+
+endmodule
